bc7b4af7787eb1ba04a86812f5d2557e95bd0c83
Jihyun_Kim <jihyun3313@daum.net>
2024-01-15 23:04:52 +0900

    feat: Form Component 생성

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "./Form.module.scss";
+const Form = ({ title }) => {
+  return (
+    <form className={styles.form}>
+      <div>
+        <input type="email" placeholder="E-mail" />
+        <div>
+          <span className={styles.form_error}></span>
+        </div>
+      </div>
 
-const Form = () => {
-  return <div>Form</div>;
+      <div>
+        <input type="password" placeholder="Password" />
+        <div>
+          <span className={styles.form_error}></span>
+        </div>
+      </div>
+
+      <button type="submit">{title}</button>
+
+      <span className={styles.form_error}></span>
+    </form>
+  );
 };
 
 export default Form;
